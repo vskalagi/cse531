810a12ae3e462df9eb034d34d3082728aeb33630
Vishwas Kalagi <vqk5180@psu.edu>
2023-03-12 13:05:42 -0400

    LAB2 upload

---
+++ Lab2/apsp.cc
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstdlib>
+#define INF 200
+
+int main(int argc, char** argv) {
+    int n, m, *d;
+    // input
+    FILE *infile = fopen(argv[1], "r");
+    fscanf(infile, "%d %d", &n, &m);
+    d = (int *) malloc(sizeof(int *) * n * n);
+    for (int i = 0; i < n * n; ++i) d[i] = INF;
+    int a, b, w;
+    for (int i = 0; i < m; ++i) {
+        fscanf(infile, "%d %d %d", &a, &b, &w);
+        d[a * n + b] = d[b * n + a] = w;
+    }
+    fclose(infile);
+    // TODO
+    // ouput
+    FILE *outfile = fopen(argv[2], "w");
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            fprintf(outfile, "%d%s",
+                (i == j ? 0 : d[i * n + j]),
+                (j == n - 1 ? " \n" : " ")
+            );
+        }
+    }
+    free(d);
+}
