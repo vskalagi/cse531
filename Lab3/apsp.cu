54c1c9b3b3ac8134ac46bc5b1731649e4362bc35
Vishwas Kalagi <vqk5180@psu.edu>
2023-04-20 01:16:26 -0400

    made few minor changes

--- Lab3/apsp.cu
+++ Lab3/apsp.cu
@@ -3,16 +3,9 @@
 #include "device_launch_parameters.h"
 #include <cstdio>
 #include <cstdlib>
-//#include "cuda_apsp.cuh"
-#define BLOCK_SIZE 4
 #define INF 200
 #define MAX_DISTANCE 1 << 30 - 1
 
-/**
- * CUDA handle error, if error occurs print message and exit program
-*
-* @param error: CUDA error status
-*/
 #define HANDLE_ERROR(error) { \
     if (error != cudaSuccess) { \
         fprintf(stderr, "%s in %s at line %d\n", \
@@ -21,39 +14,10 @@
     } \
 } \
 
-/**
- * Naive CUDA kernel implementation algorithm Floyd Wharshall for APSP
- * check if path from vertex x -> y will be short using vertex u x -> u -> y
- * for all vertices in graph
- *
- * @param u: Index of vertex u
- * @param nvertex: Number of all vertex in graph
- * @param pitch: Length of row in memory
- * @param graph: Array of graph with distance between vertex on device
- * @param pred: Array of predecessors for a graph on device
- */
-static __global__
-void _naive_fw_kernel(const int u, size_t pitch, const int nvertex, int* const graph) {
-    int x = blockDim.x * blockIdx.x + threadIdx.x;
-    int y = blockDim.y * blockIdx.y + threadIdx.y;
-
-    if (y < nvertex && x < nvertex) {
-        int indexYX = y * pitch + x;
-        int indexUX = u * pitch + x;
-
-        int newPath = graph[y * pitch + u] + graph[indexUX];
-        int oldPath = graph[indexYX];
-        if (oldPath > newPath) {
-            graph[indexYX] = newPath;
-        }
-    }
-}
-
 
 static __global__
 void _blocked_fw_dependent_ph(const int blockId, size_t pitch, const int nvertex, int* const graph,int bs) {
     extern __shared__ int cacheGraph[];
-    //__shared__ int cacheGraph[BLOCK_SIZE][BLOCK_SIZE];
 
     const int idx = threadIdx.x;
     const int idy = threadIdx.y;
@@ -62,46 +26,33 @@ void _blocked_fw_dependent_ph(const int blockId, size_t pitch, const int nvertex
     const int v2 = bs * blockId + idx;
 
     int newPath;
+    int index=idy*bs+idx;
 
     const int cellId = v1 * pitch + v2;
     if (v1 < nvertex && v2 < nvertex) {
-        cacheGraph[idy*bs+idx] = graph[cellId];
+        cacheGraph[index] = graph[cellId];
     } else {
-        cacheGraph[idy*bs+idx] = MAX_DISTANCE;
+        cacheGraph[index] = MAX_DISTANCE;
     }
 
-    // Synchronize to make sure the all value are loaded in block
     __syncthreads();
 
     #pragma unroll
     for (int u = 0; u < bs; ++u) {
         newPath = cacheGraph[idy*bs+u] + cacheGraph[u*bs+idx];
 
-        // Synchronize before calculate new value
         __syncthreads();
-        if (newPath < cacheGraph[idy*bs+idx]) {
-            cacheGraph[idy*bs+idx] = newPath;
+        if (newPath < cacheGraph[index]) {
+            cacheGraph[index] = newPath;
         }
-
-        // Synchronize to make sure that all value are current
         __syncthreads();
     }
 
     if (v1 < nvertex && v2 < nvertex) {
-        graph[cellId] = cacheGraph[idy*bs+idx];
+        graph[cellId] = cacheGraph[index];
     }
 }
 
-/**
- * Blocked CUDA kernel implementation algorithm Floyd Wharshall for APSP
- * Partial dependent phase 2
- *
- * @param blockId: Index of block
- * @param nvertex: Number of all vertex in graph
- * @param pitch: Length of row in memory
- * @param graph: Array of graph with distance between vertex on device
- * @param pred: Array of predecessors for a graph on device
- */
 static __global__
 void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int nvertex, int* const graph, int bs) {
     if (blockIdx.x == blockId) return;
@@ -113,30 +64,22 @@ void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int
     int v2 = bs * blockId + idx;
     extern __shared__ int cacheGraphBase[];
     
-
-    //__shared__ int cacheGraphBase[bs][bs];
-
-    // Load base block for graph and predecessors
     int cellId = v1 * pitch + v2;
+    int index=idy*bs+idx;
 
     if (v1 < nvertex && v2 < nvertex) {
-        cacheGraphBase[idy*bs+idx] = graph[cellId];
+        cacheGraphBase[index] = graph[cellId];
     } else {
-        cacheGraphBase[idy*bs+idx] = MAX_DISTANCE;
+        cacheGraphBase[index] = MAX_DISTANCE;
     }
 
-    // Load i-aligned singly dependent blocks
     if (blockIdx.y == 0) {
         v2 = bs * blockIdx.x + idx;
     } else {
-   // Load j-aligned singly dependent blocks
         v1 = bs * blockIdx.x + idy;
     }
 
-    //__shared__ int cacheGraph[BLOCK_SIZE][BLOCK_SIZE];
     int *cacheGraph = bs*bs + cacheGraphBase;
-
-    // Load current block for graph and predecessors
     int currentPath;
 
     cellId = v1 * pitch + v2;
@@ -145,13 +88,10 @@ void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int
     } else {
         currentPath = MAX_DISTANCE;
     }
-    cacheGraph[idy*bs+idx] = currentPath;
-
-    // Synchronize to make sure the all value are saved in cache
+    cacheGraph[index] = currentPath;
     __syncthreads();
 
     int newPath;
-    // Compute i-aligned singly dependent blocks
     if (blockIdx.y == 0) {
         #pragma unroll
         for (int u = 0; u < bs; ++u) {
@@ -160,17 +100,13 @@ void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int
             if (newPath < currentPath) {
                 currentPath = newPath;
             }
-            // Synchronize to make sure that all threads compare new value with old
             __syncthreads();
 
-           // Update new values
-            cacheGraph[idy*bs+idx] = currentPath;
+            cacheGraph[index] = currentPath;
 
-           // Synchronize to make sure that all threads update cache
             __syncthreads();
         }
     } else {
-    // Compute j-aligned singly dependent blocks
         #pragma unroll
         for (int u = 0; u < bs; ++u) {
             newPath = cacheGraph[idy*bs+u] + cacheGraphBase[u*bs+idx];
@@ -179,13 +115,10 @@ void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int
                 currentPath = newPath;
             }
 
-            // Synchronize to make sure that all threads compare new value with old
             __syncthreads();
 
-           // Update new values
-            cacheGraph[idy*bs+idx] = currentPath;
+            cacheGraph[index] = currentPath;
 
-           // Synchronize to make sure that all threads update cache
             __syncthreads();
         }
     }
@@ -195,16 +128,6 @@ void _blocked_fw_partial_dependent_ph(const int blockId, size_t pitch, const int
     }
 }
 
-/**
- * Blocked CUDA kernel implementation algorithm Floyd Wharshall for APSP
- * Independent phase 3
- *
- * @param blockId: Index of block
- * @param nvertex: Number of all vertex in graph
- * @param pitch: Length of row in memory
- * @param graph: Array of graph with distance between vertex on device
- * @param pred: Array of predecessors for a graph on device
- */
 static __global__
 void _blocked_fw_independent_ph(const int blockId, size_t pitch, const int nvertex, int* const graph,int bs) {
     if (blockIdx.x == blockId || blockIdx.y == blockId) return;
@@ -214,41 +137,37 @@ void _blocked_fw_independent_ph(const int blockId, size_t pitch, const int nvert
 
     const int v1 = blockDim.y * blockIdx.y + idy;
     const int v2 = blockDim.x * blockIdx.x + idx;
+    int index=idy*bs+idx;
 
     extern __shared__ int cacheGraphBaseRow[];
     int *cacheGraphBaseCol = bs*bs+cacheGraphBaseRow;
-    //__shared__ int cacheGraphBaseRow[BLOCK_SIZE][BLOCK_SIZE];
-    //__shared__ int cacheGraphBaseCol[BLOCK_SIZE][BLOCK_SIZE];
 
     int v1Row = bs * blockId + idy;
     int v2Col = bs * blockId + idx;
 
-    // Load data for block
     int cellId;
     if (v1Row < nvertex && v2 < nvertex) {
         cellId = v1Row * pitch + v2;
 
-        cacheGraphBaseRow[idy*bs+idx] = graph[cellId];
+        cacheGraphBaseRow[index] = graph[cellId];
     }
     else {
-        cacheGraphBaseRow[idy*bs+idx] = MAX_DISTANCE;
+        cacheGraphBaseRow[index] = MAX_DISTANCE;
     }
 
     if (v1  < nvertex && v2Col < nvertex) {
         cellId = v1 * pitch + v2Col;
-        cacheGraphBaseCol[idy*bs+idx] = graph[cellId];
+        cacheGraphBaseCol[index] = graph[cellId];
     }
     else {
-        cacheGraphBaseCol[idy*bs+idx] = MAX_DISTANCE;
+        cacheGraphBaseCol[index] = MAX_DISTANCE;
     }
 
-    // Synchronize to make sure the all value are loaded in virtual block
    __syncthreads();
 
    int currentPath;
    int newPath;
 
-   // Compute data for block
    if (v1  < nvertex && v2 < nvertex) {
        cellId = v1 * pitch + v2;
        currentPath = graph[cellId];
@@ -264,41 +183,20 @@ void _blocked_fw_independent_ph(const int blockId, size_t pitch, const int nvert
    }
 }
 
-
-
-
-/**
- * Allocate memory on device and copy memory from host to device
- * @param dataHost: Reference to unique ptr to graph data with allocated fields on host
- * @param graphDevice: Pointer to array of graph with distance between vertex on device
- * @param predDevice: Pointer to array of predecessors for a graph on device
- *
- * @return: Pitch for allocation
- */
 static
 size_t _cudaMoveMemoryToDevice(const int*  dataHost, int **graphDevice, int nvertex) {
     size_t height = nvertex;
     size_t width = height * sizeof(int);
     size_t pitch;
 
-    // Allocate GPU buffers for matrix of shortest paths d(G) 
     HANDLE_ERROR(cudaMallocPitch(graphDevice, &pitch, width, height));
 
-    // Copy input from host memory to GPU buffers and
     HANDLE_ERROR(cudaMemcpy2D(*graphDevice, pitch,
             dataHost, width, width, height, cudaMemcpyHostToDevice));
 
     return pitch;
 }
 
-/**
- * Copy memory from device to host and free device memory
- *
- * @param graphDevice: Array of graph with distance between vertex on device
- * @param predDevice: Array of predecessors for a graph on device
- * @param dataHost: Reference to unique ptr to graph data with allocated fields on host
- * @param pitch: Pitch for allocation
- */
 static
 void _cudaMoveMemoryToHost(int *graphDevice,  int* dataHost, size_t pitch,int nvertex) {
     size_t height = nvertex;
@@ -309,43 +207,9 @@ void _cudaMoveMemoryToHost(int *graphDevice,  int* dataHost, size_t pitch,int nv
     HANDLE_ERROR(cudaFree(graphDevice));
 }
 
-/**
- * Naive implementation of Floyd Warshall algorithm in CUDA
- *
- * @param dataHost: Reference to unique ptr to graph data with allocated fields on host
- */
-void cudaNaiveFW(int *dataHost,int nvertex) {
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    HANDLE_ERROR(cudaSetDevice(0));
-    //int nvertex = dataHost->nvertex;
-
-    // Initialize the grid and block dimensions here
-    dim3 dimGrid((nvertex - 1) / BLOCK_SIZE + 1, (nvertex - 1) / BLOCK_SIZE + 1, 1);
-    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
-
-    int *graphDevice, *predDevice;
-    size_t pitch = _cudaMoveMemoryToDevice(dataHost, &graphDevice, nvertex);
-
-    cudaFuncSetCacheConfig(_naive_fw_kernel, cudaFuncCachePreferL1);
-    for(int vertex = 0; vertex < nvertex; ++vertex) {
-        _naive_fw_kernel<<<dimGrid, dimBlock>>>(vertex, pitch / sizeof(int), nvertex, graphDevice );
-    }
-
-    // Check for any errors launching the kernel
-    HANDLE_ERROR(cudaGetLastError());
-    HANDLE_ERROR(cudaDeviceSynchronize());
-    _cudaMoveMemoryToHost(graphDevice, dataHost, pitch, nvertex);
-}
-
-/**
- * Blocked implementation of Floyd Warshall algorithm in CUDA
- *
- * @param data: unique ptr to graph data with allocated fields on host
- */
 void cudaBlockedFW(int *dataHost,int nvertex,int bs) {
     HANDLE_ERROR(cudaSetDevice(0));
-    //int nvertex = nvertex;
-    printf("%d-bb",bs);
+    //printf("%d-bb",bs);
     int *graphDevice, *predDevice;
     size_t pitch = _cudaMoveMemoryToDevice(dataHost, &graphDevice, nvertex);
 
@@ -361,20 +225,16 @@ void cudaBlockedFW(int *dataHost,int nvertex,int bs) {
     
 
     for(int blockID = 0; blockID < numBlock; ++blockID) {
-        // Start dependent phase
         _blocked_fw_dependent_ph<<<gridPhase1, dimBlockSize,shared_mem_size_dependent>>>
                 (blockID, pitch / sizeof(int), nvertex, graphDevice,bs);
 
-        // Start partially dependent phase
         _blocked_fw_partial_dependent_ph<<<gridPhase2, dimBlockSize, shared_mem_size_partial>>>
                 (blockID, pitch / sizeof(int), nvertex, graphDevice,bs);
 
-        // Start independent phase
         _blocked_fw_independent_ph<<<gridPhase3, dimBlockSize,shared_mem_size_partial>>>
                 (blockID, pitch / sizeof(int), nvertex, graphDevice,bs);
     }
 
-    // Check for any errors launching the kernel
     HANDLE_ERROR(cudaGetLastError());
     HANDLE_ERROR(cudaDeviceSynchronize());
     _cudaMoveMemoryToHost(graphDevice, dataHost, pitch, nvertex);
@@ -394,10 +254,9 @@ int main(int argc, char** argv) {
         d[a * n + b] = d[b * n + a] = w;
     }
     fclose(infile);
-    //cudaNaiveFW(d,n);
-    printf("%s",argv[3]);
-    int ki=atoi(argv[3]);
-    cudaBlockedFW(d,n,ki);
+    //printf("%s",argv[3]);
+    int bs=atoi(argv[3]);
+    cudaBlockedFW(d,n,bs);
     // ouput
     FILE *outfile = fopen(argv[2], "w");
     for (int i = 0; i < n; ++i) {
