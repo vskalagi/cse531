6407a76327adbf59f26d57326250b0ea9be74263
Vishwas Kalagi <vqk5180@psu.edu>
2023-04-22 21:57:15 -0400

    delete

---
+++ Lab3/apsp.cu
@@ -0,0 +1,253 @@
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+#define INF 200
+using namespace std;
+using namespace std::chrono;
+
+
+
+static __global__
+void self_dependent(const int block_n, size_t pitch, const int n_nodes, int* const matrix_data,int bs) {
+    extern __shared__ int shared_data_[];
+
+    const int idx = threadIdx.x;
+    const int idy = threadIdx.y;
+
+    const int v1 = bs * block_n + idy;
+    const int v2 = bs * block_n + idx;
+
+    int newPath;
+    int index=idy*bs+idx;
+
+    const int entry = v1 * pitch + v2;
+    if (v1 < n_nodes && v2 < n_nodes) {
+        shared_data_[index] = matrix_data[entry];
+    } else {
+        shared_data_[index] = INF;
+    }
+
+    __syncthreads();
+
+    
+    for (int u = 0; u < bs; ++u) {
+        newPath = shared_data_[idy*bs+u] + shared_data_[u*bs+idx];
+
+        __syncthreads();
+        if (newPath < shared_data_[index]) {
+            shared_data_[index] = newPath;
+        }
+        __syncthreads();
+    }
+
+    if (v1 < n_nodes && v2 < n_nodes) {
+        matrix_data[entry] = shared_data_[index];
+    }
+}
+
+static __global__
+void pivot_row_column(const int block_n, size_t pitch, const int n_nodes, int* const matrix_data, int bs) {
+    if (block_nx.x == block_n) return;
+
+    const int idx = threadIdx.x;
+    const int idy = threadIdx.y;
+
+    int v1 = bs * block_n + idy;
+    int v2 = bs * block_n + idx;
+    extern __shared__ int shared_data_Base[];
+    
+    int entry = v1 * pitch + v2;
+    int index=idy*bs+idx;
+
+    if (v1 < n_nodes && v2 < n_nodes) {
+        shared_data_Base[index] = matrix_data[entry];
+    } else {
+        shared_data_Base[index] = INF;
+    }
+
+    if (block_nx.y == 0) {
+        v2 = bs * block_nx.x + idx;
+    } else {
+        v1 = bs * block_nx.x + idy;
+    }
+
+    int *shared_data_ = bs*bs + shared_data_Base;
+    int currentPath;
+
+    entry = v1 * pitch + v2;
+    if (v1 < n_nodes && v2 < n_nodes) {
+        currentPath = matrix_data[entry];
+    } else {
+        currentPath = INF;
+    }
+    shared_data_[index] = currentPath;
+    __syncthreads();
+
+    int newPath;
+    if (block_nx.y == 0) {
+        
+        for (int u = 0; u < bs; ++u) {
+            newPath = shared_data_Base[idy*bs+u] + shared_data_[u*bs+idx];
+
+            if (newPath < currentPath) {
+                currentPath = newPath;
+            }
+            __syncthreads();
+
+            shared_data_[index] = currentPath;
+
+            __syncthreads();
+        }
+    } else {
+        
+        for (int u = 0; u < bs; ++u) {
+            newPath = shared_data_[idy*bs+u] + shared_data_Base[u*bs+idx];
+
+            if (newPath < currentPath) {
+                currentPath = newPath;
+            }
+
+            __syncthreads();
+
+            shared_data_[index] = currentPath;
+
+            __syncthreads();
+        }
+    }
+
+    if (v1 < n_nodes && v2 < n_nodes) {
+        matrix_data[entry] = currentPath;
+    }
+}
+
+static __global__
+void other_blocks(const int block_n, size_t pitch, const int n_nodes, int* const matrix_data,int bs) {
+    if (block_nx.x == block_n || block_nx.y == block_n) return;
+
+    const int idx = threadIdx.x;
+    const int idy = threadIdx.y;
+
+    const int v1 = blockDim.y * block_nx.y + idy;
+    const int v2 = blockDim.x * block_nx.x + idx;
+    int index=idy*bs+idx;
+
+    extern __shared__ int shared_data_BaseRow[];
+    int *shared_data_BaseCol = bs*bs+shared_data_BaseRow;
+
+    int same_row = bs * block_n + idy;
+    int same_col = bs * block_n + idx;
+
+    int entry;
+    if (same_row < n_nodes && v2 < n_nodes) {
+        entry = same_row * pitch + v2;
+
+        shared_data_BaseRow[index] = matrix_data[entry];
+    }
+    else {
+        shared_data_BaseRow[index] = INF;
+    }
+
+    if (v1  < n_nodes && same_col < n_nodes) {
+        entry = v1 * pitch + same_col;
+        shared_data_BaseCol[index] = matrix_data[entry];
+    }
+    else {
+        shared_data_BaseCol[index] = INF;
+    }
+
+   __syncthreads();
+
+   int currentPath;
+   int newPath;
+
+   if (v1  < n_nodes && v2 < n_nodes) {
+       entry = v1 * pitch + v2;
+       currentPath = matrix_data[entry];
+
+       
+       for (int u = 0; u < bs; ++u) {
+           newPath = shared_data_BaseCol[idy*bs+u] + shared_data_BaseRow[u*bs+idx];
+           if (currentPath > newPath) {
+               currentPath = newPath;
+           }
+       }
+       matrix_data[entry] = currentPath;
+   }
+}
+
+void cudaBlockedFW(int *cpu_data,int n_nodes,int bs) {
+    cudaSetDevice(0);
+    //printf("%d-bb",bs);
+    int *gpu_data;
+    size_t pitch ;
+    /* host to device */
+    size_t columns=n_nodes * sizeof(int);
+    cudaMallocPitch(&gpu_data, &pitch, columns, (size_t)n_nodes);
+    cudaMemcpy2D(gpu_data, pitch,cpu_data, columns, columns, (size_t)n_nodes, cudaMemcpyHostToDevice);
+
+    int n_block = (n_nodes - 1) / bs + 1;
+
+    dim3 phase1(1 ,1, 1);
+    dim3 phase2(n_block, 2 , 1);
+    dim3 phase3(n_block, n_block , 1);
+    dim3 dimBlockSize(bs, bs, 1);
+    unsigned shared_mem_size_dependent = (bs*bs*sizeof(int));
+    unsigned shared_mem_size_partial = (2*bs*bs*sizeof(int));	
+
+    
+
+    for(int block_n = 0; block_n < n_block; ++block_n) {
+        self_dependent<<<phase1, dimBlockSize,shared_mem_size_dependent>>>
+                (block_n, pitch / sizeof(int), n_nodes, gpu_data,bs);
+
+        pivot_row_column<<<phase2, dimBlockSize, shared_mem_size_partial>>>
+                (block_n, pitch / sizeof(int), n_nodes, gpu_data,bs);
+
+        other_blocks<<<phase3, dimBlockSize,shared_mem_size_partial>>>
+                (block_n, pitch / sizeof(int), n_nodes, gpu_data,bs);
+    }
+
+    cudaGetLastError();
+    cudaDeviceSynchronize();
+    /* device to host */
+    cudaMemcpy2D(cpu_data, columns, gpu_data, pitch, columns, (size_t)n_nodes, cudaMemcpyDeviceToHost);
+    cudaFree(gpu_data);
+}
+
+
+int main(int argc, char** argv) {
+    int n, m, *d;
+    // input
+    FILE *infile = fopen(argv[1], "r");
+    fscanf(infile, "%d %d", &n, &m);
+    d = (int *) malloc(sizeof(int *) * n * n);
+    for (int i = 0; i < n * n; ++i) d[i] = INF;
+    int a, b, w;
+    for (int i = 0; i < m; ++i) {
+        fscanf(infile, "%d %d %d", &a, &b, &w);
+        d[a * n + b] = d[b * n + a] = w;
+    }
+    fclose(infile);
+    //printf("%s",argv[3]);
+    int bs=atoi(argv[3]);
+    auto start= high_resolution_clock::now();
+    cudaBlockedFW(d,n,bs);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: "<< duration.count() << " microseconds" << endl;
+    // ouput
+    FILE *outfile = fopen(argv[2], "w");
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            fprintf(outfile, "%d%s",
+                (i == j ? 0 : d[i * n + j]),
+                (j == n - 1 ? " \n" : " ")
+            );
+        }
+    }
+    free(d);
+}
